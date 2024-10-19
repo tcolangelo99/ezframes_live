1afd19433cacba4dde8433774cd3b3a2d4693487
Thomas Colangelo <t.colangelo99@gmail.com>
2024-10-19 16:34:31 +0200

    removed internal folder

--- _internal/Cutie/dist/interactive_demo/_internal/torch/version.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-__all__ = ['__version__', 'debug', 'cuda', 'git_version', 'hip']
-__version__ = '2.4.0+cu118'
-debug = False
-cuda: Optional[str] = '11.8'
-git_version = 'e4ee3be4063b7c430974252fdf7db42273388d86'
-hip: Optional[str] = None
--- _internal/cv2/version.py
+++
@@ -1,5 +0,0 @@
-opencv_version = "4.8.0+c4a576a"
-contrib = True
-headless = False
-rolling = False
-ci_build = False
