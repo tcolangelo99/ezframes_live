12cc51b3da2e824448a6a6563715e9fa8289e26a
Thomas Colangelo <t.colangelo99@gmail.com>
2024-11-05 23:49:35 +0100

    launcher fix

--- setup_v2.bat
+++ setup_v2.bat
@@ -3,10 +3,6 @@ setlocal
 
 REM Set the target directory to Program Files
 set "target_dir=%ProgramFiles%\ezframes_live"
-set "internal_folder=%target_dir%\_internal"
-set "tar_file_name=%target_dir%\_internal.tar"
-set "s3_url=https://ezframesinternal.s3.eu-north-1.amazonaws.com/_internal.tar"
-set "download_path=%USERPROFILE%\Desktop\_internal.tar"
 
 REM Check if Git is installed
 git --version >nul 2>&1
@@ -26,42 +22,6 @@ if exist "%target_dir%\.git" (
     cd "%ProgramFiles%"
     git clone https://github.com/tcolangelo99/ezframes_live.git
     cd ezframes_live
-)
-
-REM Check if the _internal folder already exists
-if exist "%internal_folder%" (
-    echo _internal folder already exists. Skipping download and extraction.
-) else (
-    REM Check if _internal.tar already exists
-    if exist "%tar_file_name%" (
-        echo _internal.tar already exists. Proceeding to extraction...
-    ) else (
-        REM Download _internal.tar using curl
-        echo Downloading _internal.tar to the Desktop using curl with parallel connections...
-        curl --parallel -o "%download_path%" "%s3_url%"
-
-        REM Move the tar file to the target directory
-        if exist "%download_path%" (
-            echo Download completed successfully.
-            move /Y "%download_path%" "%tar_file_name%"
-        ) else (
-            echo Error: Failed to download _internal.tar. Access is denied or another issue occurred.
-            pause
-            exit /b
-        )
-    )
-
-    REM Extract the tar file using tar
-    echo Extracting files using tar...
-    tar -xf "%tar_file_name%" -C "%target_dir%"
-    if %ERRORLEVEL% EQU 0 (
-        echo Extraction completed successfully.
-        del "%tar_file_name%"
-    ) else (
-        echo Error: Failed to extract _internal.tar. Ensure tar is available on your system.
-        pause
-        exit /b
-    )
 )
 
 REM Check if the Visual Studio C++ Redistributable is installed
